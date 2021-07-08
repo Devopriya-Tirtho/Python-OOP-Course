18d367442211a2bd2a19bb9bc90fad273de12ef1
Devopriya-Tirtho <83715265+Devopriya-Tirtho@users.noreply.github.com>
2021-07-09 01:41:29 +0600

    Add files via upload

---
+++ Section4_Instances3.py
@@ -0,0 +1,5 @@
+class Backpack
+    def __int__(self, color, size):
+        self.items=[]
+        self.color=color
+        self.size=size
---
+++ Section4_Rectangle.py
@@ -0,0 +1,4 @@
+class Rectangle:
+    def __int__(self, length, width):
+        self.length=length
+        self.width=width
